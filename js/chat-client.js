138a784754d828302c63f644fa65c057d8e2b134
Hack Reactor Students <github@hackreactor.com>
2013-05-10 17:28:58 -0700

    refactored code and cleaned it up.

--- js/chat-client.js
+++ js/chat-client.js
@@ -1,93 +1,90 @@
-var room = "newtestroom324";
+var room = "messages";
 var memory = {};
 var friends = {};
+
 var changeroom = function(newRoom){
   room = newRoom || "messages";
+  $('.current-chat-room').text("Chatting in: " +room);
+  $('#userMessages').html("");
+  memory = {};
 };
 
-
 var receiveIt = function(){
-$.ajax('https://api.parse.com/1/classes/'+room, {
-  contentType: 'application/json',
-      success: function(data){
-        displayMessages(data);
-      },
+  $.ajax({
+    url:'https://api.parse.com/1/classes/'+room,
+    contentType: 'application/json',
+    data: {"order":"-createdAt","limit":"20"},
+    success: function(data){
+      displayMessages(data);
+    },
     error: function(data) {
       console.log('Ajax request failed');
     }
+  });
+
+  var boldFriends = function(){
+    $('.username').each(function(index){
+        $(this)[friends[$(this).text()] ? "addClass" : "removeClass"]("friend");
     });
+    displayFriends();
+  };
 
-    var boldFriends = function(){
-      $('.username').each(function(index){
-        if(friends[$(this).text()]===true) {
-          $(this).addClass("friend");
-        } else {
-          $(this).removeClass("friend");
-        }
-      });
-    };
+  var displayFriends = function(){
+    $('#friends-list').html("");
+    for(var key in friends){
+      $('#friends-list').append($('<li></li>').text(key));
+    }
+  };
 
-    var displayMessages = function(data) {
-      for(var i = 0; i < data.results.length; i++) {
-        if(!memory.hasOwnProperty(data.results[i].objectId)){
-          memory[data.results[i].objectId] = true;
-          $litag = $('<li class = "chatMessage"></li>');
-          $msg = $('<span class="message"></span>');
-          $msg.text(data.results[i].text);
-          $username = $('<span class="username" data-username="' + data.results[i].username + '"></span>');
-          $username.text(data.results[i].username);
-          $username.on('click', function(){
-            if(friends[$(this).text()] === true){
-              friends[$(this).text()] = false;
-            }else {
-              friends[$(this).text()] = true;
-            }
-            boldFriends();
-          });
-          $litag.prepend($username);
-          $litag.append(": ");
-          $litag.append($msg);
-          $('#userMessages').append($litag);
-        }
+  var displayMessages = function(data) {
+    for(var i = data.results.length-1; i >= 0; i--) {
+      if(!memory.hasOwnProperty(data.results[i].objectId)){
+        memory[data.results[i].objectId] = true;
+        $litag = $('<li class = "chatMessage"></li>');
+        $msg = $('<span class="message"></span>');
+        $msg.text(data.results[i].text);
+        $username = $('<span class="username" data-username="' + data.results[i].username + '"></span>');
+        $username.text(data.results[i].username);
+        $username.on('click', function(){
+          if(friends[$(this).text()] === true){
+            delete friends[$(this).text()];
+          }else {
+            friends[$(this).text()] = true;
+          }
+          boldFriends();
+        });
+        $createdDate = $('<span class="createdAt"></span>');
+        $createdDate.text(moment(data.results[i].createdAt).fromNow());
+        $litag.append($username,": ",$msg," -- ",$createdDate);
+        $('#userMessages').append($litag);
+        $('#main ul').animate({ scrollTop: $('#main ul').prop("scrollHeight") - $('#main ul').height() }, 1);
       }
-    };
+    }
+  };
 };
 
-  var postIt = function(message) {
-      console.log(message.text);
-      $.ajax('https://api.parse.com/1/classes/'+room,
-      {contentType: "application/json",
-      data: JSON.stringify(message),
-      type: "POST",
-      success: function(){
-        console.log("message sent");
-      }
-    });
-  };
-
-
-$(document).ready(function(){
-
-  setInterval(receiveIt, 500);
-  receiveIt();
-  var writeMsgVal = $('#writeMsg').val();
-
-  $('#sendMsg').on('click',function(){
-    var message = {
-    'username': window.location.search.slice(window.location.search.indexOf('=')+1),
-    'text': $('#writeMsg').val()
-    //'roomname': '4chan' // Optional
-    //'hax': 'alert("hi")' // Optional; used in an extra credit option below
-  };
-    postIt(message);
+var postIt = function(message) {
+    $.ajax('https://api.parse.com/1/classes/'+room,
+    {contentType: "application/json",
+    data: JSON.stringify(message),
+    type: "POST"
   });
+};
+
+setInterval(receiveIt, 500);
+
+$('#sendMsg').on('click',function(){
+  var message = {
+  'username': window.location.search.slice(window.location.search.indexOf('=')+1),
+  'text': $('#writeMsg').val(),
+  'roomname': 'tucker'
+};
+  postIt(message);
+});
 $('#receiveMsg').on('click',function(){
     receiveIt();
   });
-
-
-$('.username').on('click', function(event){
-  console.log("HDGHJSKHK");
-});
-
+$('#submitChangeRoom').on('click', function(event){
+  changeroom(prompt("Where to?"));
+  receiveIt();
 });
